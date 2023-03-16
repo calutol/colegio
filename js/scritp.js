388f17a3e27544e3ca92cdbd9f66be247d7239b9
calutol <110926906+calutol@users.noreply.github.com>
2023-03-15 23:10:31 -0400

    delete

--- js/scritp.js
+++
@@ -1,7 +0,0 @@
-let sidebar = document.querySelector(".sidebari");
-let sidebarBtn = document.querySelector(".sidebarBtn");
-
-sidebarBtn.onclick = function() {
-	sidebar.classList.toggle("active");
-}
-
